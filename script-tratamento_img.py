a73037623896d1f9b6d899056daed99f1870239d
Michel Victor <michelvictor.doc@gmail.com>
2019-04-23 10:31:02 -0300

    primeira versão funcionando

--- script-tratamento_img.py
+++ script-tratamento_img.py
@@ -1,7 +1,7 @@
 ## crie uma pasta com o nome 'base', na raiz do projeto onde esse escript vai ser executado
 import numpy as np
-import scipy
-from scipy import ndimage
+from PIL import Image
+import imageio
 import matplotlib.pyplot as plt
 import scipy.io as sio
 import os
@@ -13,32 +13,47 @@ nx = num_px*num_px*3
 m = 120
 data = np.zeros((nx,m))
 
-rootPath = os.path.abspath('./base')# raiz da base de imagens
-dir = os.listdir(rootPath)# lista todos arquivos/pastas do diretorio ./base
+ROOT_PATH = os.path.abspath('./base')#retorna o caminho completo da pasta ./base
+dir = os.listdir(ROOT_PATH)# lista todos arquivos/pastas do diretorio ./base
 
 labels = {}
 
-# cria os labels relacionando um dicetorio com um número
-count = 1
-for i in dir:
 
-    if os.path.isdir(rootPath + '/'+i):
-        labels[i] = count
+count = 1
+for pasta in dir:
+    if os.path.isdir(ROOT_PATH + '/' + pasta):
+        labels[pasta] = count # cria os labels relacionando um dicetorio com um número
         count += 1
+
+        for pathImg in glob.glob(ROOT_PATH + '/' + pasta + '/*' + EXTESAO_IMG):
+            imageNumpy = imageio.imread(pathImg)
+            print("shape antes: ", imageNumpy.shape)
+            # We preprocess the image to fit your algorithm.
+            my_image = np.array(Image.fromarray(imageNumpy).resize((num_px, num_px)))
+            print("shape depois: ", my_image.shape)
+            plt.imshow(my_image)
+            plt.show()
+            break
+
+
 
 print(labels)
 
-for i in labels:
-    imagens = glob.glob(i+EXTESAO_IMG)
-    print(imagens)
-
-#     my_image = str(i+1)+".jpeg"   # change this to the name of your image file
-#     # We preprocess the image to fit your algorithm.
-#     fname = "base/" + my_image
-#     image = np.array(ndimage.imread(fname, flatten=False))
-#     my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
-#     data[:,i] = np.squeeze(my_image)
-#     # plt.imshow(image)
-#     # plt.show()
+# for label in labels:
+#     imagens = glob.glob(rootPath+'/'+label+'/*'+EXTESAO_IMG)
+#     print(imagens[0])
 #
-# sio.savemat('data',dict(data=data))
+#     for img in imagens:
+#         imageNumpy = imageio.imread(img)
+#         print("shape antes: ", imageNumpy.shape)
+#         # We preprocess the image to fit your algorithm.
+#         my_image = np.array(Image.fromarray(imageNumpy).resize((num_px, num_px)))
+#         print("shape depois: ", my_image.shape)
+#         plt.imshow(my_image)
+#         plt.show()
+#         break
+#     # sio.savemat('data',dict(data=data))
+#
+#
+#
+#     #  my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
